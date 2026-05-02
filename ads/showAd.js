ba5f2c0212c103aa528844a68368540584b8b6af
Judedude57 <judedude57@gmail.com>
2026-05-01 21:24:22 -0400

    Update showAd.js

--- ads/showAd.js
+++ ads/showAd.js
@@ -1,1 +1,12 @@
+// /ads/showAd.js
+function showAd() {
+    const s = document.createElement("script");
+    s.dataset.zone = "10951295";
+    s.src = "https://n6wxm.com/vignette.min.js";
+    document.body.appendChild(s);
+}
 
+function showAdThen(callback, delay = 1500) {
+    try { showAd(); } catch(e) {}
+    setTimeout(callback, delay);
+}
