6c380efa7a5a3b8c20783ed16d316f7e867ce848
Judedude57 <judedude57@gmail.com>
2026-05-01 21:21:49 -0400

    Add global ads loader script

--- ads/loader.js
+++ ads/loader.js
@@ -1,1 +1,8 @@
+// /ads/loader.js
+(function loadGlobalAds() {
+    const s = document.createElement("script");
+    s.dataset.zone = "10951295";
+    s.src = "https://n6wxm.com/vignette.min.js";
+    document.body.appendChild(s);
+})();
 
