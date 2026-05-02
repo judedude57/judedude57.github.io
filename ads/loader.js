ab791e28508a666e5bfefa1e94a9079263c0465a
Judedude57 <judedude57@gmail.com>
2026-05-02 10:48:46 -0400

    Update loader.js

--- ads/loader.js
+++ ads/loader.js
@@ -1,8 +1,27 @@
-// /ads/loader.js
-(function loadGlobalAds() {
+// /Ads/adloader.js
+(function loadInfinitePlayAds() {
+    // Create the MultiTag script
     const s = document.createElement("script");
-    s.dataset.zone = "10951295";
-    s.src = "https://n6wxm.com/vignette.min.js";
+    s.src = "https://quge5.com/88/tag.min.js";
+    s.dataset.zone = "235358";
+    s.async = true;
+    s.setAttribute("data-cfasync", "false");
     document.body.appendChild(s);
-})();
 
+    // Optional: refresh function for modal ads
+    window.refreshAdSlot = function (slotId) {
+        const slot = document.getElementById(slotId);
+        if (!slot) return;
+
+        // Clear old content
+        slot.innerHTML = "";
+
+        // Re-inject MultiTag script into this slot
+        const scr = document.createElement("script");
+        scr.src = "https://quge5.com/88/tag.min.js";
+        scr.dataset.zone = "235358";
+        scr.async = true;
+        scr.setAttribute("data-cfasync", "false");
+        slot.appendChild(scr);
+    };
+})();
